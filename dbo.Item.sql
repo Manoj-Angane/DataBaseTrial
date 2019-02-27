28f82153dc0dd6c0bda81faabd668d42442016e6
Manoj Angane <manojangane@gmail.com>
2019-02-26 21:33:40 -0500

    Add files via upload
    V1

---
+++ dbo.Item.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Item] (
+    [Item_Id]    INT        NOT NULL,
+    [Item_Name]  NCHAR (10) NULL,
+    [Item_Stock] NCHAR (10) NULL,
+    [Item_entry] DATE       NULL,
+    [Store_Id]   INT        NOT NULL,
+    PRIMARY KEY CLUSTERED ([Item_Id] ASC), 
+    CONSTRAINT [FK_Item_ToTable] FOREIGN KEY (Store_Id) REFERENCES [Store]([Store_Id])
+);
+
---
+++ dbo.Store.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Store] (
+    [Store_Id]   INT        NOT NULL,
+    [Store_name] NCHAR (10) NULL,
+    [Store_Loc]  NCHAR (10) NULL,
+    PRIMARY KEY CLUSTERED ([Store_Id] ASC)
+);
+
